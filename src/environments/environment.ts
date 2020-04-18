14d0bc819c237565b3d8328144f9ba7aded62ef1
Jeferson Guardezi <jguardezi@ext.vectoritcgroup.com>
2020-04-18 16:48:07 -0300

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA9QkGAyK5jW1GyZNZihY6a_2nWD5hRplQ",
+    authDomain: "livros-a-jato.firebaseapp.com",
+    databaseURL: "https://livros-a-jato.firebaseio.com",
+    projectId: "livros-a-jato",
+    storageBucket: "livros-a-jato.appspot.com",
+    messagingSenderId: "923074645948",
+    appId: "1:923074645948:web:767e41d222973d623265b9",
+    measurementId: "G-LKWRS9K5NJ"
+  }
 };
 
 /*
