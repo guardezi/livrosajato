72830030f1ae5689bdc8d45156911d817f0d1ce7
Jeferson Guardezi <jguardezi@ext.vectoritcgroup.com>
2020-04-18 17:27:38 -0300

    finalmente corrigido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirePerformanceModule } from "@angular/fire/performance";
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+// import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirePerformanceModule
+    // AngularFireAuthModule,
+    // AngularFireAuthGuardModule,
+    // HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
