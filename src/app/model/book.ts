531d4274295813c526bc1fdd3ad174df74a8f1f1
Jeferson Guardezi <jguardezi@ext.vectoritcgroup.com>
2020-04-19 20:34:47 -0300

    ajustes na home

---
+++ src/app/model/book.ts
@@ -0,0 +1,7 @@
+export interface Books {
+    title: string;
+    category: string;
+    imgUrl: string;
+    pages: number;
+    preview: string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Books } from 'src/app/model/book';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-pages-home',
@@ -8,9 +10,19 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  private booksReference: AngularFirestoreCollection<Books>;
+  books: Observable<Books[]>;
+
+  constructor(
+    private afs: AngularFirestore,
+    private storage: AngularFireStorage,
+  ) {
+    this.booksReference = afs.collection<Books>('books');
+    this.books = this.booksReference.valueChanges();
+  }
 
   ngOnInit() {
+
   }
 
 }
